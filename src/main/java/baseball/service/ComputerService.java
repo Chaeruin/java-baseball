5783a2459ae5a281e0af39780840b707b3e00408
Chaeruin <cms3604@naver.com>
2024-11-20 14:06:41 +0900

    feat: implements computer number features

--- src/main/java/baseball/service/ComputerService.java
+++ src/main/java/baseball/service/ComputerService.java
@@ -1,4 +1,27 @@
 package baseball.service;
 
+import baseball.domain.Computer;
+import camp.nextstep.edu.missionutils.Randoms;
+import java.util.ArrayList;
+import java.util.List;
+
 public class ComputerService {
+    public List<Computer> getComputerNumber(List<Integer> computer) {
+        List<Computer> computers = new ArrayList<>();
+        for (Integer com: computer) {
+            computers.add(new Computer(com));
+        }
+        return computers;
+    }
+
+    public List<Integer> getRandomNumber() {
+        List<Integer> computer = new ArrayList<>();
+        while (computer.size() < 3) {
+            int randomNumber = Randoms.pickNumberInRange(1, 9);
+            if (!computer.contains(randomNumber)) {
+                computer.add(randomNumber);
+            }
+        }
+        return computer;
+    }
 }
